12c189a97f8f110c7c138817fda445d27bfbe048
Kenny Hom <kenny.hom27@gmail.com>
2023-12-03 18:37:18 -0500

    day3

--- 2023/advent_3/index.js
+++ 2023/advent_3/index.js
@@ -15,35 +15,114 @@ const nums = {
   9: true,
 };
 
-const symbols = {
-  "%": true,
-  $: true,
-  "=": true,
-  "+": true,
-  "*": true,
-  "/": true,
-  "@": true,
-  "-": true,
-  "&": true,
-  "#": true,
-};
+// const symbols = {
+//   "%": true,
+//   $: true,
+//   "=": true,
+//   "+": true,
+//   "*": true,
+//   "/": true,
+//   "@": true,
+//   "-": true,
+//   "&": true,
+//   "#": true,
+// };
 
-function calc() {
+// function calc() {
+//   let total = 0;
+
+//   for (let i = 0; i < format.length; i++) {
+//     let hasSymbol = false;
+//     let num = "";
+//     for (let j = 0; j < format[i].length; j++) {
+//       let cur = format[i][j];
+
+//       if (num.length && !nums[cur]) {
+//         if (hasSymbol) {
+//           total += Number(num);
+//         }
+//         hasSymbol = false;
+//         num = "";
+//         continue;
+//       }
+
+//       if (nums[cur]) {
+//         num += cur;
+
+//         if (i > 0) {
+//           const top = i > 0 && format[i - 1][j];
+//           if (symbols[top]) hasSymbol = true;
+
+//           if (j > 0) {
+//             const topLeft = format[i - 1][j - 1];
+//             if (symbols[topLeft]) hasSymbol = true;
+//           }
+
+//           if (j < format[i].length - 1) {
+//             const topRight = format[i - 1][j + 1];
+//             if (symbols[topRight]) {
+//               hasSymbol = true;
+//             }
+//           }
+//         }
+
+//         if (j > 0) {
+//           const left = format[i][j - 1];
+//           if (symbols[left]) hasSymbol = true;
+//         }
+
+//         if (j < format[i].length - 1) {
+//           const right = format[i][j + 1];
+//           if (symbols[right]) hasSymbol = true;
+//         }
+
+//         if (i < format.length - 1) {
+//           const bot = i > 0 && format[i + 1][j];
+//           if (symbols[bot]) hasSymbol = true;
+
+//           if (j > 0) {
+//             const botLeft = format[i + 1][j - 1];
+//             if (symbols[botLeft]) hasSymbol = true;
+//           }
+
+//           if (j < format[i].length - 1) {
+//             const botRight = format[i + 1][j + 1];
+//             if (symbols[botRight]) hasSymbol = true;
+//           }
+//         }
+//       }
+//     }
+
+//     if (hasSymbol) {
+//       total += Number(num);
+//     }
+//   }
+
+//   return total;
+// }
+
+const symbol = { "*": true };
+
+function calc2() {
   let total = 0;
+  const map = {};
 
   for (let i = 0; i < format.length; i++) {
-    let hasSymbol = false;
+    let symbolCoord = "";
     let num = "";
-    for (let j = 0; j < format[i].length; j++) {
+    for (let j = 0; j <= format[i].length; j++) {
       let cur = format[i][j];
 
-      if (num.length && !nums[cur]) {
-        if (hasSymbol) {
-          total += Number(num);
+      if (num && !nums[cur]) {
+        if (symbolCoord) {
+          if (!map[symbolCoord]) {
+            map[symbolCoord] = [];
+          }
+
+          map[symbolCoord].push(num);
         }
-        hasSymbol = false;
+        symbolCoord = "";
         num = "";
-        continue;
       }
 
       if (nums[cur]) {
@@ -51,54 +130,75 @@ function calc() {
 
         if (i > 0) {
           const top = i > 0 && format[i - 1][j];
-          if (symbols[top]) hasSymbol = true;
+          if (symbol[top]) {
+            symbolCoord = `${i - 1}-${j}`;
+          }
 
           if (j > 0) {
             const topLeft = format[i - 1][j - 1];
-            if (symbols[topLeft]) hasSymbol = true;
+            if (symbol[topLeft]) {
+              symbolCoord = `${i - 1}-${j - 1}`;
+            }
           }
 
-          if (j < format[i].length - 1) {
+          if (j < format[i].length) {
             const topRight = format[i - 1][j + 1];
-            if (symbols[topRight]) {
-              hasSymbol = true;
+            if (symbol[topRight]) {
+              symbolCoord = `${i - 1}-${j + 1}`;
             }
           }
         }
 
         if (j > 0) {
           const left = format[i][j - 1];
-          if (symbols[left]) hasSymbol = true;
+          if (symbol[left]) {
+            symbolCoord = `${i}-${j - 1}`;
+          }
         }
 
-        if (j < format[i].length - 1) {
+        if (j < format[i].length) {
           const right = format[i][j + 1];
-          if (symbols[right]) hasSymbol = true;
+          if (symbol[right]) {
+            symbolCoord = `${i}-${j + 1}`;
+          }
         }
 
         if (i < format.length - 1) {
           const bot = i > 0 && format[i + 1][j];
-          if (symbols[bot]) hasSymbol = true;
+          if (symbol[bot]) {
+            symbolCoord = `${i + 1}-${j}`;
+          }
 
           if (j > 0) {
             const botLeft = format[i + 1][j - 1];
-            if (symbols[botLeft]) hasSymbol = true;
+            if (symbol[botLeft]) {
+              symbolCoord = `${i + 1}-${j - 1}`;
+            }
           }
 
-          if (j < format[i].length - 1) {
+          if (j < format[i].length) {
             const botRight = format[i + 1][j + 1];
-            if (symbols[botRight]) hasSymbol = true;
+            if (symbol[botRight]) {
+              symbolCoord = `${i + 1}-${j + 1}`;
+            }
           }
         }
       }
     }
+  }
 
-    if (hasSymbol) {
-      total += Number(num);
+  console.log(map);
+
+  for (let key in map) {
+    if (map[key].length === 2) {
+      const first = Number(map[key][0]);
+      const sec = Number(map[key][1]);
+
+      total += first * sec;
     }
   }
 
   return total;
 }
 
-console.log(calc());
+console.log(calc2());
